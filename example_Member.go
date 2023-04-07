b516141e2d7c48888981836fa2337a1234c09c16
minhtuan <looongtommy27@gmail.com>
2023-04-07 10:19:13 +0700

    EXAMPLE

--- example_Member.go
+++ example_Member.go
@@ -12,35 +12,52 @@ type Member struct {
 }
 
 var (
-	numMember = 10000
+	numMember = 10
 
 	memberQueue = make(chan Member)
 )
 
-func checkVerify() {
+func checkVerify(results chan<- string) {
 	for member := range memberQueue {
-		fmt.Printf("Checking member %s\n", member.Name)
+		if member.Name != "Waiting to end" {
+			fmt.Printf("Checking member %s\n", member.Name)
+		}
 
 		time.Sleep(time.Second * 3)
 
 		if member.Verified == false {
 			member.Verified = true
 		}
-
-		fmt.Printf("Member %s verified\n", member.Name)
+		results <- member.Name
+		if member.Name != "Waiting to end" {
+			fmt.Printf("Member %s verified\n", member.Name)
+		}
 	}
 }
 
 func main() {
-	numWorkers := 10
-
+	numWorkers := 2
+	resultQueue := make(chan string, numMember)
 	for i := 1; i <= numWorkers; i++ {
-		go checkVerify()
+		go checkVerify(resultQueue)
 	}
 
 	for i := 1; i <= numMember; i++ {
 		strName := strconv.Itoa(i)
 		memberQueue <- Member{Name: strName, Verified: false}
 	}
+
+	endTask := Member{Name: "Waiting to end"}
+	fmt.Println(endTask.Name)
+
+	memberQueue <- endTask
+	for result := range resultQueue {
+		if result == endTask.Name {
+			break
+		}
+	}
+
+	close(memberQueue)
+	close(resultQueue)
 
 }
